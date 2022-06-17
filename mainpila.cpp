3914f039bc2f1b5291affcae00eed30a620bf616
DeniseRea <105187806+DeniseRea@users.noreply.github.com>
2022-06-17 11:37:54 -0500

    Update mainpila.cpp

--- mainpila.cpp
+++ mainpila.cpp
@@ -1,13 +1,12 @@
 /*UFA-ESPE
 * Deber 2 Parcial 2
 * Autores: Armas Daniel - Iza Christopher - Rea Denise - Rivadeneira Ricardo
-* Fecha de creación 15/06/2022
-* Fecha de modificación 17/06/2022
+* Fecha de creaciÃ³n 15/06/2022
+* Fecha de modificaciÃ³n 17/06/2022
 * Grupo 15 - 20
 * github del grupo @dnrea@espe.edu.ec @cdiza5@espe.edu.ec daarmas10@espe.edu.ec rxrivadeneira1@espe.edu.ec
 */
 
-//#include "ingresoDatos.h"
 using namespace std;
 #include <string>
 #include "transformacion.h"
