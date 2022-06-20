fbfa4166edba2c2e54d4096aa548fea98e5a7f59
DeniseRea <105187806+DeniseRea@users.noreply.github.com>
2022-06-20 09:55:48 -0500

    Update CalculadoraV2.cpp

--- Version2/CalculadoraV2.cpp
+++ Version2/CalculadoraV2.cpp
@@ -1,3 +1,11 @@
+/*UFA-ESPE
+* Deber 2 Parcial 2
+* Autores: Armas Daniel - Iza Christopher - Rea Denise - Rivadeneira Ricardo
+* Fecha de creación 15/06/2022
+* Fecha de modificación 20/06/2022
+* Grupo 15 - 20
+* github del grupo @dnrea@espe.edu.ec @cdiza5@espe.edu.ec daarmas10@espe.edu.ec rxrivadeneira1@espe.edu.ec
+*/
 #include <iostream>
 #include "Transformacion.cpp"
 
